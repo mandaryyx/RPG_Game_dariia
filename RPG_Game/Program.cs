5847deb336f34183baf5464cce2193f951e8a65b
mandaryyx <daryna.dobrianska@gmail.com>
2025-12-02 13:03:22 +0200

    Add Character adn IEquipabble

--- RPG_Game/Program.cs
+++ RPG_Game/Program.cs
@@ -1,5 +1,19 @@
 ﻿namespace RPG_Game
 {
+    public interface IEquippable
+    {
+        string Name { get; }
+        void Equip();
+        void Unequip();
+    }
+    public abstract class Character
+    {
+        private string _name;
+        private int _health;
+        private int _maxHealth;
+        private int _strenght;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -8,3 +22,4 @@
         }
     }
 }
+                                   
