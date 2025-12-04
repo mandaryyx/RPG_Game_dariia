ee5815ff06e1314d5bb2f38ae07925966a5d1151
mandaryyx <daryna.dobrianska@gmail.com>
2025-12-04 12:05:51 +0200

    Make script Characer

--- RPG_Game/Program.cs
+++ RPG_Game/Program.cs
@@ -6,13 +6,6 @@
         void Equip();
         void Unequip();
     }
-    public abstract class Character
-    {
-        private string _name;
-        private int _health;
-        private int _maxHealth;
-        private int _strenght;
-    }
 
     internal class Program
     {
--- RPG_Game/obj/Debug/net8.0/RPG_Game.AssemblyInfo.cs
+++ RPG_Game/obj/Debug/net8.0/RPG_Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5847deb336f34183baf5464cce2193f951e8a65b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
